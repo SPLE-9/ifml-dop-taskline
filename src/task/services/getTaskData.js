d2770962e6f3b9ea7e48b0594d1d1fe19aad05db
Maria Aurellia <88271313+mariaadannies@users.noreply.github.com>
2025-06-03 14:07:16 +0700

    fix(task): get task data endpoint

--- src/task/services/getTaskData.js
+++ src/task/services/getTaskData.js
@@ -7,7 +7,7 @@ const getTaskData = (params = {}) => {
 	const { getToken } = tokenManager();
 	const token = getToken();
 	let paramsGet = Object.assign(params, {token});
-	return axios.get(`${environment.rootApi}/call//detail`, {
+	return axios.get(`${environment.rootApi}/call/task/detail`, {
 		params: paramsGet,		
 		headers: {
 			'Authorization': token,
